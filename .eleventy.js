e3447874b430ff649570484dd69a17ec9a1e4cc3
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2019-03-29 16:43:49 +0100

    chore(eleventy): added base config

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+module.exports = (eleventyConfig) => {
+  // Copy the `img/` directory 
+  // eleventyConfig.addPassthroughCopy("assets")
+  
+  return {
+    // passthroughFileCopy: true,
+    // markdownTemplateEngine: "jstl"
+  }
+}
