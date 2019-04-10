99185834d949055ebd2000cc7e5c396590f98b47
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2019-04-10 22:25:24 +0200

    refactor(): removed obsolete config/dependencies

--- .eleventy.js
+++
@@ -1,9 +0,0 @@
-module.exports = (eleventyConfig) => {
-  // Copy the `img/` directory 
-  // eleventyConfig.addPassthroughCopy("assets")
-  
-  return {
-    // passthroughFileCopy: true,
-    // markdownTemplateEngine: "jstl"
-  }
-}
